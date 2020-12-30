e13da49069f1275ecd5d25127b24282e29d74631
Rodgers Nyangweso <47724155+nyangweso-rodgers@users.noreply.github.com>
2020-12-30 11:51:46 +0300

    using the calender module

--- Functions_and_Modules_in_Python/calender.py
+++ Functions_and_Modules_in_Python/calender.py
@@ -1,5 +1,13 @@
-import calender
-# print(dir(calender))
+import calendar
+print(dir(calender))
 days = {
-    0:
+    0: 'Monday',
+    1: 'Tuesday',
+    2: 'Wednesday',
+    3: 'Thursday',
+    4: 'Friday',
+    5: 'Saturday',
+    6: 'Sunday',
 }
+day_in_week = calendar.weekday(2020,12,30)
+print(days[day_in_week]) # Wednesday
