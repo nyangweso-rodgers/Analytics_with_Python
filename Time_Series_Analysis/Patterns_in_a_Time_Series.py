4806696ce18fed9e5495c140f21a44bb99dbe7a5
Rodgers Nyangweso <47724155+nyangweso-rodgers@users.noreply.github.com>
2021-01-01 21:36:49 +0300

    python file illustrating patterns in Time Series

---
+++ Time_Series_Analysis/Patterns_in_a_Time_Series.py
@@ -0,0 +1,18 @@
+# Patterns in a Time Series
+from dateutil.parser import parse 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+plt.rcParams.update({'figure.figsize': (10, 7), 'figure.dpi': 120})
+
+fig, axes = plt.subplots(1,3, figsize=(20,4), dpi=100)
+pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/guinearice.csv', parse_dates=['date'], 
+            index_col='date').plot(title='Trend Only', legend=False, ax=axes[0])
+
+pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/sunspotarea.csv', parse_dates=['date'], 
+            index_col='date').plot(title='Seasonality Only', legend=False, ax=axes[1])
+
+pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/AirPassengers.csv', parse_dates=['date'], 
+            index_col='date').plot(title='Trend and Seasonality', legend=False, ax=axes[2])
